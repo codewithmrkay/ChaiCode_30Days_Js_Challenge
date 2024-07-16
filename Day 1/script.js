9aaa3ba1cdca2b689490ec57b8f6fb1e6485a929
karan <kkadam58707@gmail.com>
2024-07-16 23:04:37 +0530

    all files uploads

---
+++ Day 1/script.js
@@ -0,0 +1,18 @@
+console.log('jai shree ram')
+// var num = 3
+// let naam = 'chai'
+// const hot =true
+// let obj = {
+//     pogo:'chota bheem'
+// }
+// let arr = [1,2,3]
+// console.log(naam,num,hot)
+// console.log(typeof num,typeof naam,typeof hot,typeof obj,typeof arr)
+// let naam = 'chai'
+// console.log(naam)
+// naam = 'code'
+// console.log(naam)
+const num = 99
+console.log(num)
+// num =100
+console.log(typeof num , )
---
+++ Day 2/script.js
@@ -0,0 +1,34 @@
+console.log('jai shree ram')
+//?------ task 1 to 5 [ arithmetic operator ]
+let a = 12
+let b  = 2
+console.log(a+b)//direct methoed
+console.log(a-b)//direct methoed
+console.log(a*b)//direct methoed
+console.log(a/b)//direct methoed
+console.log(a%b)//direct methoed
+//?------ task 6 and 7
+a+=5
+b-=5
+console.log(a,b)
+//?------ task 8,9,10 [ comarison operator ]
+if(a>b){ console.log('yes bachaa a>b')}
+if(a>=b){ console.log('yes bachaa a>=b')}
+if('5'==5){console.log('yes babdi "5"==5')}
+if('5'===5){console.log('yes babdi "5"===5')}
+else{console.log('no babdi "5"===5')}
+//?------ task 11 to 13 [ logically operator ]
+if(5==5 && 3==2){console.log('both are true')}
+else{console.log('gdbd')}
+if(5==5 || 3==2){console.log('op')}
+else{console.log('gdbd')}
+//?------ task 14 [ ternary operator ]
+let result
+result = (9==9)?'op':'gdbd' 
+console.log(result)
+if(9==9){
+    console.log('op')
+}
+else{
+    console.log('gdbd')
+}
